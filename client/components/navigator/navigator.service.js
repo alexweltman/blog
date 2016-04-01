2ef23f5c5955a5768bb23586181bb0db60f6f4ec
Alex <alexander.weltman@colorado.edu>
2016-03-31 23:29:15 -0600

    removed unused service

--- client/components/navigator/navigator.service.js
+++ client/components/navigator/navigator.service.js
@@ -4,11 +4,7 @@ angular.module('blogApp')
   .factory('navigator', ['$state', function ($state) {
     return {
       goToPost : function(postId){
-        console.log('router go to post');
         $state.go("post", {"postId" : postId});
-      },
-      log : function() {
-        console.log('fuck you');
       }
     }
   }]);
